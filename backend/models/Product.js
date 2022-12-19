4e7a77561af6cd82d4052e8baf1785155165f454
Arnob <arnobdam@gmail.com>
2022-12-19 11:53:08 -0500

    made product model

---
+++ backend/models/Product.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    image: {
+        type: String
+    }
+  }, {
+    timestamps: true
+  });
+
+  module.exports = mongoose.model('Product', productSchema);
