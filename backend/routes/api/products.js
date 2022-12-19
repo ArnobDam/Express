89a3e6f5cf1a94e45d06af0296d02eda13a5caf9
Yong Kim <yhkim4573@gmail.com>
2022-12-19 11:46:34 -0500

    products router get products, product, edit product, delete product

--- backend/routes/api/products.js
+++ backend/routes/api/products.js
@@ -1,8 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
+// GET ALL PRODUCTS
 router.get('/', function(req, res, next) {
   res.json({ message: "GET /products" });
+});
+
+// GET PRODUCT
+router.get('/:product_id', function(req, res, next) {
+  res.json({ message: "GET /product" });
+});
+
+// MAKE PRODUCT
+router.post('/new', function(req, res, next) {
+  res.json({ message: "POST /product" });
+});
+
+// UPDATE PRODUCT
+router.patch('/:product_id', function(req, res, next) {
+  res.json({ message: "PATCH /product" });
+});
+
+// UPDATE PRODUCT
+router.put('/:product_id', function(req, res, next) {
+  res.json({ message: "PUT /product" });
+});
+
+// DELETE PRODUCT
+router.delete('/:product_id', function(req, res, next) {
+  res.json({ message: "DELETE /product" });
 });
 
 module.exports = router;
