5424d9d0a398d80acbd069d55d0171aef7af97f0
kkj2010 <kunjukim0210@gmail.com>
2022-12-20 12:06:37 -0500

    add dashboard

--- frontend/src/features/dashboard/components/Dashboard.jsx
+++ frontend/src/features/dashboard/components/Dashboard.jsx
@@ -1,6 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../../store/session";
 import "./Dashboard.css";
+import { NavLink } from "react-router-dom";
+import { TbLogout } from "react-icons/tb";
+import { AiFillHome, AiFillSetting } from "react-icons/ai";
+import { FaClipboardList } from "react-icons/fa";
+import { MdFastfood } from "react-icons/md";
+import { RiChatHistoryFill } from "react-icons/ri";
+import { GoGraph } from "react-icons/go";
+import { BiSearch } from "react-icons/bi";
 
 {
   /* <h1>Dashboard</h1>
@@ -15,13 +23,48 @@ export function Dashboard() {
   return (
     <div className="Dashboard">
       <div className="left">
-        <h1>EXPRESS</h1>
-        <input type="search" />
-        <h2>Dashboard</h2>
-        <div>
-          <p>Link1</p>
-          <p>Link2</p>
-          <p>Link3</p>
+        <div
+          className="brand-logo"
+          style={{ width: "120px", height: "100px" }}
+        />
+        <div className="searchbar-container">
+        <input
+          className="searchbar"
+          type="search"
+          placeholder="What you want?"
+        />
+        <BiSearch className="search-icon" />
+        </div>
+        <h2 className="sidebar-dashboard">Dashboard</h2>
+        <div className="sidebar-links-container">
+          <div className="sidebar-link">
+            <AiFillHome className="link-icon" />
+            Home
+          </div>
+          <div className="sidebar-link">
+            <MdFastfood className="link-icon" />
+            Menu
+          </div>
+          <div className="sidebar-link">
+            <FaClipboardList className="link-icon" />
+            Order
+          </div>
+          <div className="sidebar-link">
+            <RiChatHistoryFill className="link-icon" />
+            History
+          </div>
+          <div className="sidebar-link">
+            <GoGraph className="link-icon" />
+            Report
+          </div>
+          <div className="sidebar-link">
+            <AiFillSetting className="link-icon" />
+            Settings
+          </div>
+          <div className="logout-button" onClick={() => dispatch(logout())}>
+            <TbLogout className="logout-icon" />
+            <div className="logout">Logout</div>
+          </div>
         </div>
       </div>
       <div className="center">
