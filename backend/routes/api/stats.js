0af13178395b780622e62f09c29d4c5a76e2d06f
Arnob <arnobdam@gmail.com>
2022-12-20 17:23:38 -0500

    create total orders and total product sold routes

--- backend/routes/api/stats.js
+++ backend/routes/api/stats.js
@@ -25,6 +25,52 @@ router.get("/revenue", async (req, res, next) => {
     }
 });
 
+// GET TOTAL # OF ORDERS
+router.get("/number-of-orders", async (req, res, next) => {
+
+    try {
+        const orders = await Order.find();
+
+        let totalNumberOfOrders = orders.length;
+
+        // for (const order of orders) {
+        //     totalRevenue += order.totalPrice;
+        // }
+
+        return res.json(totalNumberOfOrders);
+    } catch (err) {
+        const error = new Error("No records found");
+        error.statusCode = 404;
+        error.errors = { message: "You have no avaialble orders at this time." };
+        return next(error);
+    }
+});
+
+//GET TOTAL # OF PRODUCTS SOLD
+router.get("/number-of-products-sold", async (req, res, next) => {
+
+    try {
+        const orders = await Order.find();
+
+        let totalNumberOfProductsSold = 0;
+
+        for (const order of orders) {
+            for (const product of order.products) {
+                totalNumberOfProductsSold += product.quantity;
+            }
+            
+        }
+
+        return res.json(totalNumberOfProductsSold);
+    } catch (err) {
+        const error = new Error("No records found");
+        error.statusCode = 404;
+        error.errors = { message: "You have no avaialble orders at this time." };
+        return next(error);
+    }
+});
+
+
 
 
 module.exports = router;
