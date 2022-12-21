e4165a2cfac0592e4972fdaac7ba98e148915915
chazzlee <chazz@dev.com>
2022-12-21 14:42:31 -0500

    remove local incrementers

--- frontend/src/features/dashboard/components/Cart.jsx
+++ frontend/src/features/dashboard/components/Cart.jsx
@@ -1,20 +1,10 @@
 import "./Cart.css";
 import foodimage from "./sandwich.png";
 import { RiDeleteBin5Fill, RiMoneyDollarCircleFill } from "react-icons/ri";
-import {BsFillCreditCard2BackFill} from "react-icons/bs"
-import {MdQrCode} from "react-icons/md"
-import { useState } from "react";
-
+import { BsFillCreditCard2BackFill } from "react-icons/bs";
+import { MdQrCode } from "react-icons/md";
 
 export function Cart() {
-  const [counter, setCounter]= useState(1)
-  const incrementCounter=()=> setCounter(counter+1)
-  let decrementCounter=()=> setCounter(counter-1)
-
-  if (counter<=0){
-    decrementCounter=()=> setCounter(0)
-  }
-
   return (
     <>
       <div className="cart-container">
@@ -34,9 +24,13 @@ export function Cart() {
                 <div className="order-item-price">$9.10</div>
                 <div className="qty-change-section">
                   <div className="qty-button">
-                    <button className="minus-button" onClick={decrementCounter}>-</button>
-                    <span className="qty">{counter}</span>
-                    <button className="plus-button" onClick={incrementCounter}>+</button>
+                    <button className="minus-button" onClick={() => {}}>
+                      -
+                    </button>
+                    <span className="qty">1</span>
+                    <button className="plus-button" onClick={() => {}}>
+                      +
+                    </button>
                   </div>
                   <span className="delete-button">
                     <RiDeleteBin5Fill />
@@ -53,9 +47,13 @@ export function Cart() {
                 <div className="order-item-price">$9.10</div>
                 <div className="qty-change-section">
                   <div className="qty-button">
-                    <button className="minus-button" onClick={decrementCounter}>-</button>
-                    <span className="qty">{counter}</span>
-                    <button className="plus-button" onClick={incrementCounter}>+</button>
+                    <button className="minus-button" onClick={() => {}}>
+                      -
+                    </button>
+                    <span className="qty">3</span>
+                    <button className="plus-button" onClick={() => {}}>
+                      +
+                    </button>
                   </div>
                   <span className="delete-button">
                     <RiDeleteBin5Fill />
@@ -72,9 +70,13 @@ export function Cart() {
                 <div className="order-item-price">$9.10</div>
                 <div className="qty-change-section">
                   <div className="qty-button">
-                    <button className="minus-button" onClick={decrementCounter}>-</button>
-                    <span className="qty">{counter}</span>
-                    <button className="plus-button" onClick={incrementCounter}>+</button>
+                    <button className="minus-button" onClick={() => {}}>
+                      -
+                    </button>
+                    <span className="qty">1</span>
+                    <button className="plus-button" onClick={() => {}}>
+                      +
+                    </button>
                   </div>
                   <span className="delete-button">
                     <RiDeleteBin5Fill />
@@ -112,15 +114,23 @@ export function Cart() {
             </div>
             <div className="payment-method">
               <div className="payment-debit">
-                <div><BsFillCreditCard2BackFill/></div>
-                <div>Debit</div></div>
-              <div className="payment-cash">
-                <div><RiMoneyDollarCircleFill/></div>
-                <div>Cash</div></div>
-              <div className="payment-code">
-                <div><MdQrCode/></div>
-                <div>Code</div>
+                <div>
+                  <BsFillCreditCard2BackFill />
                 </div>
+                <div>Debit</div>
+              </div>
+              <div className="payment-cash">
+                <div>
+                  <RiMoneyDollarCircleFill />
+                </div>
+                <div>Cash</div>
+              </div>
+              <div className="payment-code">
+                <div>
+                  <MdQrCode />
+                </div>
+                <div>Code</div>
+              </div>
             </div>
             <div className="total-container">
               <div className="sub-total">
