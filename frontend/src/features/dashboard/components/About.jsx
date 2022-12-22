9198942877e55e91b2861e5c9af9782d601584a4
Yong Kim <yhkim4573@gmail.com>
2022-12-22 11:31:29 -0500

    css about

--- frontend/src/features/dashboard/components/About.jsx
+++ frontend/src/features/dashboard/components/About.jsx
@@ -10,11 +10,16 @@ export function About() {
       <div className="about-title">
         <h1>About</h1>
       </div>
-      <div className="about-page-div">
-        <div id="scorecard" className="arnob-profile-div">arnob</div>
-        <div id="scorecard" className="chazz-profile-div">chazz</div>
-        <div id="scorecard" className="kunju-profile-div">kunju</div>
-        <div id="scorecard" className="daniel-profile-div">daniel</div>
+      <div className="main-about">
+        <div className="about-team">
+          w
+        </div>
+        <div className="about-page-div">
+          <div id="scorecard" className="arnob-profile-div">arnob</div>
+          <div id="scorecard" className="chazz-profile-div">chazz</div>
+          <div id="scorecard" className="kunju-profile-div">kunju</div>
+          <div id="scorecard" className="daniel-profile-div">daniel</div>
+        </div>
       </div>
     </>
   );
