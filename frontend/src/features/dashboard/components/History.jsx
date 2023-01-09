f59de9bebeff7c0f3083f287212bbccdb5057554
chazzlee <l.chazz00@gmail.com>
2023-01-09 18:25:46 -0500

    add no history to history page

--- frontend/src/features/dashboard/components/History.jsx
+++ frontend/src/features/dashboard/components/History.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { selectOrderHistoryList } from "../../../store/orders";
 import { HistoryCard } from "./HistoryCard";
 
+// TODO: style no history and heading
 export function History() {
   const orderHistoryList = useSelector(selectOrderHistoryList);
 
@@ -11,10 +12,11 @@ export function History() {
     <>
       <div className="history-container">
         <div style={{ paddingTop: "10px" }}></div>
-        {/* <div className="history-title">Order history</div> */}
         <div className="history-order-container">
+          <h2 className="history-title">History</h2>
+          {orderHistoryList.length === 0 && <p>No orders yet</p>}
           {orderHistoryList?.map((history) => (
-            < HistoryCard history={history} key={history._id} />
+            <HistoryCard history={history} key={history._id} />
           ))}
         </div>
       </div>
